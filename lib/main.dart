aabf603da7fd08e0f2dfc2d7bde4567cca313c32
lijealso <lijealso@gmail.com>
2023-02-05 23:46:04 +0000

    add questions

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,55 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final _questions = const [
+    {
+      'questionText': 'Q1. Who created Flutter?',
+      'answers': [
+        {'text': 'Facebook', 'score': -2},
+        {'text': 'Adobe', 'score': -2},
+        {'text': 'Google', 'score': 10},
+        {'text': 'Microsoft', 'score': -2},
+      ],
+    },
+    {
+      'questionText': 'Q2. What is Flutter?',
+      'answers': [
+        {'text': 'Android Development Kit', 'score': -2},
+        {'text': 'IOS Development Kit', 'score': -2},
+        {'text': 'Web Development Kit', 'score': -2},
+        {'text': 'SDK to build beautiful IOS, Android, Web & Desktop Native Apps', 'score': 10},
+      ],
+    },
+    {
+      'questionText': ' Q3. Which programing language is used by Flutter',
+      'answers': [
+        {'text': 'Ruby', 'score': -2},
+        {'text': 'Dart', 'score': 10},
+        {'text': 'C++', 'score': -2},
+        {'text': 'Kotlin', 'score': -2},
+      ],
+    },
+    {
+      'questionText': 'Q4. Who created Dart programing language?',
+      'answers': [
+        {'text': 'Lars Bak and Kasper Lund', 'score': 10},
+        {'text': 'Brendan Eich', 'score': -2},
+        {'text': 'Bjarne Stroustrup', 'score': -2},
+        {'text': 'Jeremy Ashkenas', 'score': -2},
+      ],
+    },
+    {
+      'questionText': 'Q5. Is Flutter for Web and Desktop available in stable version?',
+      'answers': [
+        {
+          'text': 'Yes',
+          'score': -2,
+        },
+        {'text': 'No', 'score': 10},
+      ],
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp();
